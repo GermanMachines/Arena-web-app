680c8404202bb154708a6abbe40d6dac2bd0207d
Fourat Abdellatif <fourat-abdellatif99@hotmail.fr>
2022-04-25 21:47:26 +0100

    add pagination

--- src/Controller/FrontProductController.php
+++ src/Controller/FrontProductController.php
@@ -2,20 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
 use App\Repository\ProductsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class FrontProductController extends AbstractController
 {
     /**
      * @Route("/front/product", name="app_front_product")
      */
-    public function index(ProductsRepository $productsRepository): Response
+    public function index(Request $request, PaginatorInterface $paginator): Response
     {
+        // Méthode findBy qui permet de récupérer les données avec des critères de filtre et de tri
+        $data = $this->getDoctrine()->getRepository(Products::class)->findBy([], ['id' => 'desc']);
+
+        $products = $paginator->paginate(
+            $data, // Requête contenant les données à paginer (ici nos products)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6 // Nombre de résultats par page
+        );
+
         return $this->render('product/index.html.twig', [
-            'products' => $productsRepository->findAll(),
+            'products' => $products,
         ]);
     }
 }
