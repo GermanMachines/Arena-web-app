4ad5ab60a211ef84c2f8cdf790064d12c1dd13cb
abdou sfayhi <abdousfayhi12@gmail.com>
2022-05-11 14:33:58 +0100

    php bin/console server:run works

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     CMEN\GoogleChartsBundle\CMENGoogleChartsBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
